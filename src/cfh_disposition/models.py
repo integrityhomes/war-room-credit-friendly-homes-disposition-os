b86c08ef591075aee2fd6d5ce8a1b5ae6143657c
integrityhomes <integritydealsys@gmail.com>
2026-08-05 09:55:29 -0400

    Add filled property status

--- src/cfh_disposition/models.py
+++ src/cfh_disposition/models.py
@@ -14,6 +14,7 @@ class PropertyStatus(StrEnum):
     READY = "Ready to Launch"
     LIVE = "Marketing Live"
     PENDING = "Pending"
+    FILLED = "Filled"
     SOLD = "Sold"
     PAUSED = "Paused"
 
